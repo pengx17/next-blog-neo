aa3d301ff2e1c5fe5acd50ac8a046a8c0c01b388
Peng Xiao <pengxiao@freewheel.tv>
2023-01-18 00:28:04 +0800

    remove unused files

--- app/(non-home)/posts/[slug]/post-renderer.tsx
+++ app/(non-home)/posts/[slug]/post-renderer.tsx
@@ -1,16 +1,13 @@
 import { getPostHTML, PostProperties } from "../../../../lib/data";
-import { createSectionWrapper } from "./components";
 import { Date } from "../../../date";
 import { use } from "react";
-
-const WrappedH1 = createSectionWrapper("h1");
 
 export function PostRenderer({ id, name, date }: PostProperties) {
   return (
     <>
-      <WrappedH1 className="text-4xl my-4 font-serif font-bold leading-snug">
+      <h1 className="my-6 text-4xl my-4 font-serif font-bold leading-snug">
         {name}
-      </WrappedH1>
+      </h1>
       <div className="text-gray-600 mb-8 ml-0.5">
         <Date dateString={date} />
       </div>
