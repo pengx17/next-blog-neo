6522252da8ba409941afe329bef5e7d0845abd1a
Peng Xiao <pengxiao@freewheel.tv>
2023-01-26 22:28:35 +0800

    Update page.tsx

--- app/(non-home)/tests/page.tsx
+++ app/(non-home)/tests/page.tsx
@@ -1,18 +1,14 @@
-import { getPageMD, getBlocks, getPageById } from "../../lib/notion-data";
-import { mdToCompiled } from "../../lib/md-to-compiled";
+import { getPageById } from "../../lib/notion-data";
 import { PostRenderer } from "../posts/[slug]/post-renderer";
 
 export default async function Tests() {
-  const { md, notes } = await getPageMD("08b78b3d41fa48c082b783ab3fbada18");
-
   const page = await getPageById("08b78b3d41fa48c082b783ab3fbada18");
-  const { compiledSource } = await mdToCompiled(md);
 
   return (
     <>
       <h1 className="font-serif text-4xl font-bold my-4">Tests</h1>
       <hr />
-      <PostRenderer {...page} notes={notes} compiledSource={compiledSource} />
+      <PostRenderer {...page} />
     </>
   );
 }
