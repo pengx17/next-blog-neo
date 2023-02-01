945134c7a2fcd78b032c30a33a5fc68de0e2fadc
Peng Xiao <pengxiao@freewheel.tv>
2023-02-01 15:52:45 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
   experimental: {
     // https://beta.nextjs.org/docs/upgrade-guide#step-1-creating-the-app-directory
     appDir: true,
+    // see https://github.com/shikijs/next-shiki
+    serverComponentsExternalPackages: ["shiki", "vscode-oniguruma"],
   },
   basePath: process.env.BASE_PATH ?? "",
   eslint: {
