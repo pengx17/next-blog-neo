429a62b2ba61f03c90e16c6823a90e77154c8cd9
Peng Xiao <pengxiao@freewheel.tv>
2023-02-02 17:58:50 +0800

    Update fetch-link-preview.ts

--- app/lib/fetch-link-preview.ts
+++ app/lib/fetch-link-preview.ts
@@ -2,13 +2,15 @@ import { getLinkPreview } from "link-preview-js";
 import { cache } from "react";
 
 export const fetchLinkPreview = cache(async (url: string) => {
+  const startTime = Date.now();
   console.info("fetching " + url);
   const data = await getLinkPreview(Array.isArray(url) ? url[0] : url, {
-    timeout: process.env.NODE_ENV === "development" ? 6000 : 1000,
+    timeout: process.env.NODE_ENV === "development" ? 6000 : 500,
     headers: {
       "user-agent": "googlebot",
     },
     followRedirects: "follow",
   });
+  console.info("fetched " + url + " in " + (Date.now() - startTime) + "ms");
   return data;
 });
