db1dbae83c131920bb0e2679e5558a84d01cf229
Peng Xiao <pengxiao@freewheel.tv>
2023-02-03 13:45:18 +0800

    style updates

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,8 @@
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
-  return <div>loading...</div>;
+  return (
+    <div className="fixed z-10 inset-0 bg-white flex items-center justify-center">
+      loading...
+    </div>
+  );
 }
