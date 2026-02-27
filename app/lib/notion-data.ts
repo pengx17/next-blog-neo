71de7f3b0a2569e4b46051a2ce2edeb1f1ef2767
Peng Xiao <pengxiao@outlook.com>
2026-02-27 19:18:16 +0800

    Revert "fix: add type parameter to parseProperties to fix TS error"
    This reverts commit 212533376e29e1a107a359bfcfec53cb4c63bf12.

--- app/lib/notion-data.ts
+++ app/lib/notion-data.ts
@@ -85,9 +85,7 @@ export const getPosts = cache(async (retry = 3): Promise<PostProperties[]> => {
     });
 
     const flattenedResults = (results as PageObjectResponse[]).map((r) => {
-      const props = parseProperties<PostProperties>(
-        "properties" in r ? r.properties : {}
-      );
+      const props = parseProperties("properties" in r ? r.properties : {});
       return {
         date: r.created_time,
         ...props,
